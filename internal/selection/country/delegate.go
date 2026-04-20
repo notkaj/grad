a8f975bb2156a16f926a15f180834934798674ba
kaj <notkaj@gmail.com>
2026-04-20 18:08:55 -0400

    feat: removing unnecessary keys

--- internal/selection/country/delegate.go
+++ internal/selection/country/delegate.go
@@ -15,23 +15,9 @@ func newItemDelegate() list.DefaultDelegate {
 }
 
 func update(msg tea.Msg, m *list.Model) tea.Cmd {
-	// i, ok := m.SelectedItem().(item)
-	// if !ok {
-	// 	return nil
-	// }
-
 	switch b := msg.(type) {
 	case tea.KeyPressMsg:
 		switch {
-		case key.Matches(b, listKeys.toggleSpinner):
-			return m.ToggleSpinner()
-
-		case key.Matches(b, listKeys.toggleTitleBar):
-			v := !m.ShowTitle()
-			m.SetShowTitle(v)
-			m.SetShowFilter(v)
-			m.SetFilteringEnabled(v)
-
 		case key.Matches(b, listKeys.toggleStatusBar):
 			m.SetShowStatusBar(!m.ShowStatusBar())
 
@@ -40,29 +26,18 @@ func update(msg tea.Msg, m *list.Model) tea.Cmd {
 
 		case key.Matches(b, listKeys.toggleHelpMenu):
 			m.SetShowHelp(!m.ShowHelp())
-
 		}
 	}
 	return nil
 }
 
 type listKeyMap struct {
-	toggleSpinner    key.Binding
-	toggleTitleBar   key.Binding
 	toggleStatusBar  key.Binding
 	togglePagination key.Binding
 	toggleHelpMenu   key.Binding
 }
 
 var listKeys = listKeyMap{
-	toggleSpinner: key.NewBinding(
-		key.WithKeys("s"),
-		key.WithHelp("s", "toggle spinner"),
-	),
-	toggleTitleBar: key.NewBinding(
-		key.WithKeys("T"),
-		key.WithHelp("T", "toggle title"),
-	),
 	toggleStatusBar: key.NewBinding(
 		key.WithKeys("S"),
 		key.WithHelp("S", "toggle status"),
@@ -77,25 +52,17 @@ var listKeys = listKeyMap{
 	),
 }
 
-// Additional short help entries. This satisfies the help.KeyMap interface and
-// is entirely optional.
 func (d listKeyMap) ShortHelp() []key.Binding {
 	return []key.Binding{
-		d.toggleSpinner,
-		d.toggleTitleBar,
 		d.toggleStatusBar,
 		d.togglePagination,
 		d.toggleHelpMenu,
 	}
 }
 
-// Additional full help entries. This satisfies the help.KeyMap interface and
-// is entirely optional.
 func (d listKeyMap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
 		{
-			d.toggleSpinner,
-			d.toggleTitleBar,
 			d.toggleStatusBar,
 			d.togglePagination,
 			d.toggleHelpMenu,
